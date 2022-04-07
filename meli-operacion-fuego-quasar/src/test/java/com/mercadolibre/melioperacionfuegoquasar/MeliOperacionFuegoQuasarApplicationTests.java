ffef2ece688d2a2279d38f5457858ec65f829d1e
wilmarADiaz <wadcardid@yahoo.com>
2022-04-07 16:28:07 -0500

    primer Commit

---
+++ meli-operacion-fuego-quasar/src/test/java/com/mercadolibre/melioperacionfuegoquasar/MeliOperacionFuegoQuasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.melioperacionfuegoquasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliOperacionFuegoQuasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
